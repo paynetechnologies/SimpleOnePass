3c654be6d22c79e0bbf6e5e8461592b1fc9300b3
Howard Payne <paynehd@gmail.com>
2018-10-31 19:01:33 -0400

    added global.py

---
+++ src/global.py
@@ -0,0 +1,18 @@
+BSIZE   = 128
+NONE    = -1
+EOS     = '\0'
+NUM     = 255
+DIV     = 257
+MOD     = 258
+ID      = 259
+DONE    = 260
+
+tokenval = 0
+lineno   = 0
+
+class entry(object):
+    lexptr = []
+    token = 0
+
+
+
