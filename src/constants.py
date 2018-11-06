3f256183b186a2705c5f041761ffc6697eee6738
Howard Payne <paynehd@gmail.com>
2018-11-06 08:51:17 -0500

    reordered constants

--- src/constants.py
+++ src/constants.py
@@ -2,16 +2,17 @@
 class constants:
     """ Global file declarations """
     BSIZE   = 128
-    DIV     = 257
-    DONE    = 260
+    DIV     = 256
+    DONE    = 257
     EOS     = '\0'
     EOF     = False
-    ID      = 259
+    ID      = 258
     LINE_NUMBER   = 0
-    MOD     = 258
-    NEWLINE = 998
+    MOD     = 259
+    NEWLINE = 260
     NONE    = -1
-    NUM     = 255
+    NUM     = 261
+    OPERATOR = 262
     SYMBOL_TABLE = []
     TOKEN_VALUE = 0
     WHITESPACE = 999
