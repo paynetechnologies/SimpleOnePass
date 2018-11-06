3a9a8edd26c80e326a0806e0b2d431c23fd6f728
Howard Payne <paynehd@gmail.com>
2018-11-06 08:51:58 -0500

    updated init.py

--- src/init.py
+++ src/init.py
@@ -1,10 +1,18 @@
 import sys
-import error
-from constants import constants, entry
-from symbol import insert
+import src.error
+from src.constants import constants, entry
+from src.symbol_table import lookup, insert
 
 keywords = {'div' : 'DIV', 'mod' : 'MOD'}
 
-def init():
+def init_symbol_table():
     '''Loads keywords into SYMBOL_TABLE'''
-    constants.SYMBOL_TABLE = (insert(lex, tok) for lex,tok in keywords)
+    print('init_symbol_table')
+    for lex,tok in keywords.items():
+        insert(lex, tok) 
+    #constants.SYMBOL_TABLE = (symbol_table.insert(lex, tok) for lex,tok in keywords)
+    for entry in constants.SYMBOL_TABLE:
+        print(f'id : {entry.lex} - token : {entry.token}') 
+
+if (__name__ == "__main__"):
+    init_symbol_table()
