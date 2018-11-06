dde6860a4e5993c743ce8e08eb06c35c19b15042
Howard Payne <paynehd@gmail.com>
2018-11-06 08:53:06 -0500

    refactored test_lexer.py

--- src/tests/test_lexer.py
+++ src/tests/test_lexer.py
@@ -25,55 +25,61 @@ class Test_Lexer(unittest.TestCase):
         return super().tearDown()
 
     def test_01_newline(self):
-        self.lex.loadBuffer('\n abcd ;')
+        self.lex.loadBuffer('\n;')
         token = self.lex.lex_analysis()
-        self.assertEqual(constants.NEWLINE, token)
+        #self.assertEqual(constants.NEWLINE, token)
+        self.assertEqual(constants.DONE, token)
 
     def test_02_White_Space(self):
         # '   abc ;'
-        self.lex.loadBuffer(' abc ;')
+        self.lex.loadBuffer(' ;')
         token = self.lex.lex_analysis()
-        self.assertEqual(constants.WHITESPACE, token)
+        #self.assertEqual(constants.WHITESPACE, token)
+        self.assertEqual(constants.DONE, token)
 
     def test_03_Single_Digit(self):
         # ' 1 ;'
-        self.lex.loadBuffer('1 abc ;')
+        self.lex.loadBuffer('1 ;')
         token = self.lex.lex_analysis()
+        self.assertEqual(constants.NUM, token)
 
     def test_04_Multiple_Digits(self):
         # ' 123 ;'
-        self.lex.loadBuffer('123 abc ;')
+        self.lex.loadBuffer('123 ;')
         token = self.lex.lex_analysis()
-
+        self.assertEqual(constants.NUM, token)
 
     def test_05_Alpha(self):
         # 'abc  ;'
         self.lex.loadBuffer('abc ;')
         token = self.lex.lex_analysis()
-
+        self.assertEqual(constants.ID, token)
 
     def test_06_Alphanumeric(self):
         #  'abc123 ;'
-        self.lex.loadBuffer('abc1d2e3f ;')
+        self.lex.loadBuffer('abc1d2e ;')
         token = self.lex.lex_analysis()
-
+        self.assertEqual(constants.ID, token)
 
     def test_07_EmptyString(self):
         # ';'
         self.lex.loadBuffer(';')
         token = self.lex.lex_analysis()
+        self.assertEqual(constants.DONE, token)        
 
 
     def test_08_EOF (self):
         # ';'
         self.lex.loadBuffer(';')
         token = self.lex.lex_analysis()
+        self.assertEqual(constants.DONE, token)        
 
 
     def test_09_operators(self):
         # +, -, /, *, DIV, MOD
         self.lex.loadBuffer('DIV ;')
         token = self.lex.lex_analysis()
+        self.assertEqual(constants.OPERATOR, token)        
 
 
 if __name__ == '__main__':
