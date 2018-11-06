8fc1fa8e6a6ee6473708e8c1c9c985c7f834c80c
Howard Payne <paynehd@gmail.com>
2018-11-06 18:54:04 -0500

    added clear symbol table method

--- src/init.py
+++ src/init.py
@@ -5,12 +5,17 @@ from src.symbol_table import lookup, insert
 
 keywords = {'div' : 'DIV', 'mod' : 'MOD'}
 
+def clear_symbol_table():
+    constants.SYMBOL_TABLE = []
+
 def init_symbol_table():
     '''Loads keywords into SYMBOL_TABLE'''
     print('init_symbol_table')
+
     for lex,tok in keywords.items():
         insert(lex, tok) 
-    #constants.SYMBOL_TABLE = (symbol_table.insert(lex, tok) for lex,tok in keywords)
+
+    #constants.SYMBOL_TABLE = (insert(lex, tok) for lex,tok in keywords.items())
     for entry in constants.SYMBOL_TABLE:
         print(f'id : {entry.lex} - token : {entry.token}') 
 
