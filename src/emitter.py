91082cc4e11a9efc1430e590c014b1f5c0f6da9b
Howard Payne <paynehd@gmail.com>
2018-11-07 08:06:14 -0500

    Added new files emitter and parser

--- src/emitter.py
+++ src/emitter.py
@@ -1,1 +1,16 @@
 from src.globals import constants, entry
+
+def emit(t, tval):
+    switch(t):
+        case "+": case "-": case "*": case "/":
+            print(f'\n{t}')
+        case DIV:
+            print(f'DIV\n')
+        case MOD:
+            print(f'MOD\n')            
+        case NUM:
+            print(f'{tval}\n')            
+        case ID`:
+            print(f'\n' + symbol_table[tval].lex)            
+        default:
+            print(f'token {t} tokenval {tval}')
--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,69 @@
 import sys
-import error
-from constants import constants, entry
+import src.error
+from src.constants import constants, entry
+from src.lexer import lex_manager
 
+
+def parse():
+    lookahead = lex_manager.lex_analysis()
+    while (lookahead != DONE):
+        expr(); 
+        match(';')
+
+def expr():
+    term()
+    
+    while(True):
+        switch (lookahead):
+        case '+':
+        case '-':
+            t = lookahead
+            match(lookahead)
+            term()
+            emit(t, None)
+            continue;
+        default:
+            return
+
+def term():
+    factor()
+
+    while (True)            
+        switch (lookahead):
+        case '*':
+        case '/':
+        case 'DIV':
+        case 'MOD':                
+            t = lookahead
+            match(lookahead)
+            factor()
+            emit(t, None)
+            continue;
+        default:
+            return
+
+
+def factor():
+    switch(lookahead)            :
+
+        case "(":
+            match("(")
+            expr()
+            match")"
+            break;
+
+        case NUM:
+            match(NUM, tokenval)
+            match(NUM)
+            break;
+
+        case ID:
+
+        default:
+            error("syntax error")
+
+def match(t):
+    if (lookahead ==t):
+        lookahead = lex_manager.lex_analysis()
+    else
+        error("syntax error")
