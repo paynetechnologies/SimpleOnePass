532bdc1d9c9bb9acd92655a70c33f37adeb678e8
Howard Payne <paynehd@gmail.com>
2018-11-12 11:59:34 -0500

    added COMMENTS token

--- src/constants.py
+++ src/constants.py
@@ -7,6 +7,7 @@ class constants:
     EOF     = True
     SYMBOL_TABLE = []
 
+    COMMENTS = 255
     DIV     = 256
     DONE    = 257
     ID      = 258
@@ -17,9 +18,28 @@ class constants:
     OPERATOR = 262
     WHITESPACE = 999
 
+
+
 class entry:
     """ a symbol table entry """
     def __init__(self, lex, value):
         self.lexeme = lex
         self.token = value
 
+E = {
+    "null-character":
+        "Null character in input stream, replaced with U+FFFD.",
+    "invalid-codepoint":
+        "Invalid codepoint in stream.",
+}
+
+tokenTypes = {
+    "Doctype": 0,
+    "Characters": 1,
+    "SpaceCharacters": 2,
+    "StartTag": 3,
+    "EndTag": 4,
+    "EmptyTag": 5,
+    "Comment": 6,
+    "ParseError": 7
+}
