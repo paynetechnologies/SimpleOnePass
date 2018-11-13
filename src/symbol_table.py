12a52c87ee5850caf7574b0d805e6820d0b07165
Howard Payne <paynehd@gmail.com>
2018-11-13 09:09:36 -0500

    using classmethods

--- src/symbol_table.py
+++ src/symbol_table.py
@@ -6,11 +6,14 @@ class symbol_table():
 
     SYMMAX = 100 # size of symbol table
 
-    def __init__(self):
-        super().__init__()
-        self.last_entry = -1 # last used position in symtabl
+    last_entry = -1
 
-    def st_lookup(self, value):
+    # def __init__(self):
+    #     super().__init__()
+    #     self.last_entry = -1 # last used position in symtabl
+
+    @classmethod
+    def lookup(cls, value):
         """ Returns postion of entry for s """
         #for (p = last_entry; p > 0 ; p -= 1):
         for indx, entry in reversed(list(enumerate(constants.SYMBOL_TABLE))):
@@ -19,16 +22,21 @@ class symbol_table():
                 return indx
         return None
         
-    def st_insert(self, value, token):
-        self.last_entry += 1
+    @classmethod
+    def insert(cls, value, token):
+        symbol_table.last_entry += 1
         
-        if (self.last_entry >= symbol_table.SYMMAX):
+        if (symbol_table.last_entry >= symbol_table.SYMMAX):
             error_message("Symbol Table Full")
        
         new_entry = entry(value, token)
-        constants.SYMBOL_TABLE.insert(self.last_entry, new_entry)
+        constants.SYMBOL_TABLE.insert(symbol_table.last_entry, new_entry)
+        return symbol_table.last_entry
 
 if __name__ == '__main__':
-    st = symbol_table()
-    st.st_insert('DIV', 'DIV')
-    st.st_lookup('DIV')
+    # st = symbol_table()
+    # st.symbol_table_insert('DIV', 'DIV')
+    # st.symbol_table_lookup('DIV')
+
+    symbol_table.insert('DIV', 'DIV')
+    symbol_table.lookup('DIV')    
