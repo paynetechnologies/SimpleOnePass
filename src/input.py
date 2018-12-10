651d3e35124f5d014f8a933180d077c624c43d26
Howard Payne <paynehd@gmail.com>
2018-12-10 09:34:28 -0500

    added access methods to input.py

--- src/input.py
+++ src/input.py
@@ -1,9 +1,9 @@
-'''                                              EndBuf       
-   Start_buf                                DANGER ^  END
-   ^ Next                                      ^   |   ^
-   |^                                          |   |   |
-   |                                           +   |   | 
-   ++------------------------------------------+---+---+
+'''                                               EndBuf       
+   Start_buf                                DANGER  |  END
+   | Next                                      |    |   |
+   |/                                          |    |   |
+   v                                           v    v   v 
+   +-------------------------------------------+---+---+
    |                                           |YYY|XXX| After initial read
    |-------------------------------------------|---|---|
    |                                           |   |   |
@@ -12,23 +12,23 @@
    |<-------------------- BUFSIZE -------------------->|
 '''
 '''                                              
-   Start_buf                                DANGER    END
-   ^                                     Next  ^       ^
-   |                                       ^   | EndBuf|
-   |     pmark       smark          emark  |   |   ^   | 
-   +-----^-----------^--------------^------+---+---+---+
-   |     |prev lexeme|current lexeme|          |YYY|XXX| Normally
+   Start_buf                       Next      DANGER    END
+   |     pmark    smark        emark |         |  EndBuf|
+   |     |        |            |     |         |    |   |
+   v     v        v            v     v         v    v   v 
+   +-------------------------------------------+---+---+
+   |     |prev lex|current lex|                |YYY|XXX| Normally
    |-------------------------------------------|---|---|
    |                                                   |
    |<------------------- BUFSIZE --------------------->|
 '''
 '''                                              
-   Start_buf                            DANGER        END
-   ^                           Next       ^            ^
-   |                            ^         |    EndBuf  |
-   pmark    smark       emark   |         |      |     |
-   ^--------^-----------^-------+---------+------+-----+
-   |prev lex|current lex|       YYY            |XXXXXXX| After Flush
+   Start_buf                            DANGER          END
+   |                           Next       |   EndBuf    |
+   v                            |         |     |       |
+   pmark    smark       emark   |         |     v       v
+   v--------v-----------v-------v---------v-------------
+   |prev lex|current lex|      |YYY|           |XXXXXXX| After Flush
    |-------------------------------------------|-------|
    |                           -->| n x MAXLEX |<--    |
    |<------------------- BUFSIZE --------------------->|
@@ -39,16 +39,6 @@ import array
 import time
 
 
-def open_funct(filename, mode):
-    fd=open(filename,mode)
-    return fd
-
-def close_funct(fd):
-    fd.close()
-
-def read_funct(fd):
-    Input.startBuf = fd.read(Input.BUFSIZE)
-    
 
 class Input:
 
@@ -69,9 +59,9 @@ class Input:
     pLineno = 0     # line # of previous lexeme
     pLength = 0     # length of previous lexeme
 
-    STDIN = sys.stdin
+    STDIN = sys.stdin # default standard input
 
-    inpFile     = None  # input file handle
+    inpFile     = STDIN  # input file handle
     lineNo      = 1     # current line number
     mLine       = 1     # Line # when mark_end() is called
     termChar    = 0     # holds the char that was overwritten by \0 when last char is null terminated
@@ -79,6 +69,20 @@ class Input:
 
     ii_io = {}
 
+    been_called = False
+
+#---------------------------------------------------
+#                      I/O
+#---------------------------------------------------
+def open_funct(filename, mode):
+    fd=open(filename,mode)
+    return fd
+
+def close_funct(fd):
+    fd.close()
+
+def read_funct(fd, starting_at, need):
+    Input.startBuf = fd.read(Input.BUFSIZE)
 
 def ii_io(open_funct, close_funct, read_funct):
     Input.ii_io["openp"] = open_funct
@@ -87,40 +91,106 @@ def ii_io(open_funct, close_funct, read_funct):
     print(Input.ii_io)
 
 
+#---------------------------------------------------
+#                      Access
+#---------------------------------------------------
+
+def ii_text():
+    return Input.sMark
+
+def ii_length():
+    return Input.eMark - Input.sMark
+
+def ii_lineno():
+    return Input.lineNo
+
+def ii_ptext():
+    return Input.pMark
+
+def ii_plength():
+    return Input.pLength
+
+def ii_plineno():
+    return Input.pLineno
+
+def ll_mark_start():
+    Input.mLine = Input.lineNo
+    Input.eMark = Input.sMark = Input.next
+    return Input.sMark
+
+def ii_mark_end():
+    Input.mLine = Input.lineNo
+    Input.eMark = Input.next
+    return (Input.eMark)
+
+def ii_mark_start():
+    if (Input.sMark >= Input.eMark):
+        return None
+    else:
+        return ++Input.sMark
+
+def ii_to_mark():
+    Input.lineNo = Input.mLine
+    Input.next = Input.eMark
+    return (Input.next)
+
+def ii_mark_prev():
+    '''
+    Set the pMark. Note: a buffer flush wonit' go past pMark so
+    once you sevit, you must move it every time you move sMark.
+    This is not done automatically, since you may want to 
+    remember the token before last rather than the last one.
+    If ii_pmark_prev is never called, pMark is ignored, no worries;
+    '''
+
+    Input.pMark = Input.sMark
+    Input.pLineno = Input.lineNo
+    Input.pLength = Input.eMark - Input.sMark
+    return (Input.pMark)
+
 # Flush buffer when next passes this address
 def DANGER():
     return Input.endBuf - Input.MAXLOOK
 
-
-def no_more_chars():
+def NO_MORE_CHARS():
     return (Input.EOF_Read and Input.next >= Input.endBuf)
 
 
 def ii_newfile(name=None):
+    '''
+    Prepare a new iput file for reading. If newfile() isn't called before
+    input() or input_line() then stdin is used. The current inpout file is
+    closed after successfully opening the new one (but stdin is not closed.)
+    
+    Return -1 if the file can't be opened, otherwise, return the file
+    descripotor returned from open(). Note: the old input file won't be 
+    closed unless the new file is opened successfully The error code (errno)
+    generated by the bad open() will still be valid, so you can call perror()
+    to find out what went wrong if you like. At least one free file
+    descriptor must be available when newfile() is called. Note in the open
+    call that 'rb' is used to open read in binary mode.
+    '''
 
-    ii_io(open_funct, close_funct, read_funct)
-
-    fd=None
+    fd = None # file descriptor
 
     name = input if (name == '/dev/tty') else name
 
-    #fd = Input.STDIN if name is None else open(name, mode='rb')
     fd = Input.STDIN if name is None else Input.ii_io["openp"](name, 'rb')
     if(fd != 0):   
         print(type(fd))
 
-        if Input.inpFile is not None and Input.inpFile != "STDIN":
+        if Input.inpFile != Input.STDIN:
             Input.ii_io["closep"](Input.inpFile)
 
-            Input.inpFile = fd
+        Input.inpFile = fd
+        Input.EOF_Read=0
 
-            Input.EOF_Read=0
-            Input.next = Input.END
-            Input.sMark = Input.END
-            Input.eMark = Input.END
-            Input.endBuf = Input.END
-            Input.lineNo = 1
-            Input.mLine = 1
+        Input.next = Input.END
+        Input.sMark = Input.END
+        Input.eMark = Input.END
+        Input.endBuf = Input.END
+        Input.lineNo = 1
+        Input.mLine = 1
 
     return fd
 
@@ -137,8 +207,6 @@ def readfile_into_buffer(filename):
     ii_io(open_funct, close_funct, read_funct)
 
     Input.ii_io["openp"](filename, 'rb')
-    #fd=open(filename,'rb',Input.BUFSIZE)
-    #Input.startBuf = fd.read(Input.BUFSIZE)
 
     start = time.time()
     with open(filename, 'rb') as f:
@@ -162,7 +230,155 @@ def readfile_into_buffer(filename):
     #print(f't1 - {t1} : t2  {t2}')
 
 def chunk_file(fd, chunksize=Input.BUFSIZE):
-    return iter(lambda: fd.read(chunksize), b'')                
+    return iter(lambda: fd.read(chunksize), b'')      
+
+def ii_advance():
+    '''
+    ii_advance is the real input function. It returns the next character
+    from input and advances past it. The buffer is flushed if the current
+    character is within MAXLOOK characters of the endo fhte buffer. 0 is
+    returned at the end of file. -1 returned if the buffer can't be flushed
+    because it's too full. In this case, you can call ii_flush(1) to do a
+    buffer flush bug you'll lose the curent lexeme as a result.
+    '''
+    if (not Input.been_called):
+        # push a newline on the empty buffer so LEX start-of-line
+        # will work on the first input line.
+        Input.next = Input.sMark = Input.eMark = Input.END - 1
+        Input.startBuf[Input.next]  = '\n'
+        Input.lineNo -= 1
+        Input.mLine -= 1
+        Input.been_called = True
+
+    if (NO_MORE_CHARS()):
+        return 0
+
+    if (not Input.EOF_Read and (ii_flush(0) < 0)):
+        return -1
+
+    if (Input.next == '\n'):
+        Input.lineNo += 1
+
+    Input.next +=1
+    return (Input.startBuf[Input.Next])
+
+def ii_flush(force):
+    '''
+    Flush the input buffer. Do nothing if th ecurrent input characters isn't
+    in the danger zone, otherwise move all unread characters to the left end
+    of the buffer and fill the remainder of the buffer. Note that input()
+    flushes the buffer willy-nilly if you read past the end of buffer.
+    Similarly, input_line() flushes the buffer at the beginning of each line.
+                                      
+   Start_buf                       Next      DANGER    END
+   |        pmark smark        emark |         |  EndBuf|
+   |        |     |            |     |         |    |   |
+   v        v     v            v     v         v    v   v 
+   +-------------------------------------------+---+---+
+   | this is already read | to be read next    | waste |
+   |-------------------------------------------|-------|
+   |<------ shift_amt --->|<----copy_amt------>|       |
+   |                                                   |
+   |<------------------- BUFSIZE --------------------->|
+
+    Either the pMark of sMark, whichever is smaller, is used as the leftmost
+    edge of the buffer. None of the text to the right of the mark will be 
+    lost. Return 1 if OK, -1 if buffer is full and can't be flushed, 0 if 
+    at end-of-file. If force is true, a buffer flush is forced and the 
+    characters already in it are discarded. Don't call this function on a 
+    buffer that's been terminated by ii_term()
+    '''
+    copy_amt = shift_amt = left_edge = 0
+
+    if (NO_MORE_CHARS()):
+        return 0
+
+    if (Input.EOF_Read):    # nothing more to read
+        return 1
+
+    if (Input.next >= DANGER() or force):
+        if (Input.pMark > 0):
+            left_edge =  min(Input.sMark, Input.pMark)
+        else:
+            left_edge = Input.sMark
+
+        left_edge = min(Input.sMark, Input.pMark) if Input.pMark else Input.sMark
+
+        if (shift_amt < Input.MAXLEX): # if not enough room 
+            if (not force):
+                return -1
+
+            left_edge = ii_mark_start() # reset start to current character
+            ii_mark_prev()
+
+            shift_amt = left_edge - Input.startBuf
+
+        copy_amt = Input.endBuf - left_edge
+
+        copy(Input.startBuf, left_edge, copy_amt)
+
+        if (not ii_fillBuf(Input.startBuf + copy_amt)):
+            #ferr("INTERNAL ERROR, ii_flush: Buffer full, can't read \n")
+            pass
+        
+        if (Input.pMark):
+            Input.pMark -= shift_amt
+
+        Input.sMark = shift_amt
+        Input.eMark = shift_amt
+        Input.next = shift_amt
+
+    return 1
+
+    def ii_fillBuf(starting_at):
+        '''
+        Fill the ipout buffer from starting_at to the end of the buffer.
+        The input file is not closed when EOF is reached. Buffers are read
+        in units of MAXLEX characters; it's an error if that any characters
+        cannot be read (0 is returned in this case). For example, if MAXLEX
+        is 1024, then 1024 characters will be read at a time. The number of
+        characters read is returned. Eof_read is true as soon as the last
+        buffer is read.
+
+        PORTABILITY NOTE: I'm assuming that the read function actually returns
+        the number of characters loaded into the buffer, and that that number 
+        will be < need only when the last chunk of the file is read. It's 
+        possible for read() to always return fewer than the number of requested 
+        characters in MS-DOS untranslated-input mode, however (if the File is opened 
+        without the O_BINARY flag). That's not a problem here because the file is 
+        opened in binary mode, but it could cause problems if you change from binary 
+        to text mode at some point.
+        '''
+        need = 0 # number of bytes needed from input
+        got = 0  # number of bytes actually read
+
+        need = (( Input.END  - starting_at) / Input.MAXLEX) * Input.MAXLEX
+
+        print(f'Reading {need} bytes \n')
+
+        if (need < 0):
+            #ferr("INTERNAL ERROR (ii_filBuf( : Bad rea-request starting addr. \n")
+            pass
+
+        if (need == 0):
+           return 0
+
+        got = Input.ii_io["readp"](Input.inpFile, starting_at, need)
+        if (got == -1):
+            pass
+            #ferr("Can't read input file. \n")
+
+        Input.endBuf = starting_at + got
+
+        if (got < need):
+            Input.EOF_Read = True
+
+        return got
+        
+
+def copy(buf, left, amt):
+    pass
+
 
 if __name__ == '__main__':
     #readfile_into_buffer("./test_files/web.config") #python input.py
